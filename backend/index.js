e63e2fec8064c30f8dadad898e60cd6daafffb8f
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-02 11:40:19 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -102,6 +102,10 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
   const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
