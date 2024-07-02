d4a1c57178390a6c867e750f7513b712c9ad16f9
laurikor00 <84325911+laurikor00@users.noreply.github.com>
2024-07-02 16:55:01 +0300

    Add files via upload

---
+++ backend/models/person.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const url = "mongodb+srv://fullstack:kS3t3XubEMtE5Ib2@cluster0.bxg8fsc.mongodb.net/Phonebook?retryWrites=true&w=majority";
+
+console.log('connecting to', url);
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return /^\d{2,3}-\d+$/.test(value);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Person', personSchema);
